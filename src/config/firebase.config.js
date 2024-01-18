606ba32c88654197273d0b6d05d122f5c1abb1d5
Shubham soni <ss4185667@gmail.com>
2024-01-18 23:02:25 +0530

    api

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -3,19 +3,20 @@ import {getAuth} from "firebase/auth"
 import { getFirestore} from "firebase/firestore"
 const firebaseConfig = {
 
-    apiKey:   process.env.REACT_APP_API_KEY,
-  
-    authDomain:   process.env.REACT_APP_AUTH_DOMAIN,
-  
-    projectId:  process.env.REACT_APP_PROJECTID,
-  
-    storageBucket:   process.env.REACT_APP_STORAGE_BUCKET,
-  
-    messagingSenderId:  process.env.REACT_APP_MESSAGING_SENDER_ID, 
-  
-    appId:   process.env.REACT_APP_APPID,
-  
-  };
+  apiKey: "AIzaSyB7x6RuPM1IsyAttrtAtIQye1vdqAd72vA",
+
+  authDomain: "resume-biulder.firebaseapp.com",
+
+  projectId: "resume-biulder",
+
+  storageBucket: "resume-biulder.appspot.com",
+
+  messagingSenderId: "559139474544",
+
+  appId: "1:559139474544:web:21ca7472dfd93d041d78b5"
+
+};
+
   const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
   const auth = getAuth(app);
   const db =getFirestore(app);
